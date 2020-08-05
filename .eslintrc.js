9a79e8bd5b8f61d26e913a10dbf09570b14df0d8
fence <fence@desu-mail.moe>
2020-08-05 17:32:09 +0200

    linting works

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,63 +1,83 @@
+const jsRules = {
+    indent: [2, 4, { SwitchCase: 1 }],
+    quotes: [2, "double"],
+    semi: ["error", "always"],
+    "space-before-function-paren": ["error", "never"]
+};
+
+const tsRules = {
+    ...jsRules
+};
+
+const jsExtends = [
+    "standard",
+    "plugin:import/errors",
+    "plugin:import/warnings"
+];
+
+const tsExtends = [
+    ...jsExtends,
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+];
+
+const jsPlugins = [
+    "import",
+    "prefer-arrow",
+    "jsdoc"
+];
+
+const tsPlugins = [
+    ...jsPlugins,
+    "@typescript-eslint",
+    "@angular-eslint/eslint-plugin"
+];
+
+const tsParserOptions = {
+    ecmaVersion: 10,
+    project: [
+        "tsconfig.lint.json"
+    ],
+    sourceType: "module",
+    ecmaFeatures: {
+        modules: true
+    }
+};
+
 module.exports = {
     env: {
         browser: true,
         node: true
     },
-    extends: [
-        "standard",
-        "plugin:import/errors",
-        "plugin:import/warnings"
-
-    ],
-    rules: {
-        indent: [2, 4, { SwitchCase: 1 }],
-        quotes: [2, "double"],
-        semi: ["error", "always"],
-        "space-before-function-paren": ["error", "never"]
-    },
-    plugins: [
-        "import",
-        "prefer-arrow",
-        "jsdoc"
-    ],
     overrides: [
         {
+            files: ["*.js"],
+            extends: jsExtends,
+            plugins: jsPlugins,
+            rules: jsRules
+        },
+        {
             files: ["*.ts"],
-            extends: [
-                "standard",
-                "plugin:import/errors",
-                "plugin:import/warnings",
-                "plugin:import/typescript",
-                "plugin:@typescript-eslint/eslint-recommended",
-                "plugin:@typescript-eslint/recommended",
-                "plugin:@typescript-eslint/recommended-requiring-type-checking"
-
-            ],
+            extends: tsExtends,
             parser: "@typescript-eslint/parser",
-            parserOptions: {
-                ecmaVersion: 10,
-                project: [
-                    "tsconfig.lint.json"
-                ],
-                sourceType: "module",
-                ecmaFeatures: {
-                    modules: true
-                }
-            },
-            plugins: [
-                "@typescript-eslint",
-                "@angular-eslint/eslint-plugin",
-                "import",
-                "prefer-arrow",
-                "jsdoc"
-            ],
+            parserOptions: tsParserOptions,
+            plugins: tsPlugins,
+            rules: tsRules
+        },
+        {
+            files: ["*.spec.ts"],
+            extends: tsExtends,
+            parser: "@typescript-eslint/parser",
+            parserOptions: tsParserOptions,
+            plugins: tsPlugins,
             rules: {
-                "@typescript-eslint/no-unsafe-call": 0,
-                "@typescript-eslint/no-unsafe-member-access": 0,
-                indent: [2, 4, { SwitchCase: 1 }],
-                quotes: [2, "double"],
-                semi: ["error", "always"],
-                "space-before-function-paren": ["error", "never"]
+                ...tsRules,
+                "@typescript-eslint/no-unsafe-call": "off",
+                "@typescript-eslint/no-unsafe-member-access": "off",
+                "@typescript-eslint/no-unsafe-assignment": "off",
+                "@typescript-eslint/no-floating-promises": "off"
             }
         },
         {
@@ -67,6 +87,7 @@ module.exports = {
             rules: {
                 "@angular-eslint/template/banana-in-a-box": "error",
                 "@angular-eslint/template/no-negated-async": "error"
+
             }
         }
 
